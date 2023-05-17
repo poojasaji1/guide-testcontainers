1185bb71049dd1596bbd1a190e1408cac451fa56
Morgan Chang <shin19991207@gmail.com>
2023-05-17 16:36:36 -0400

    update test to run in dev mode

--- finish/src/test/java/it/io/openliberty/guides/inventory/LibertyContainer.java
+++ finish/src/test/java/it/io/openliberty/guides/inventory/LibertyContainer.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2022, 2023 IBM Corporation and others.
+ * Copyright (c) 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -11,146 +11,29 @@
 // end::copyright[]
 package it.io.openliberty.guides.inventory;
 
-import java.io.FileInputStream;
-import java.security.KeyStore;
-import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-// imports for a JAXRS client to simplify the code
-import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
-// logger imports
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-// testcontainers imports
-// tag::GenericContainer[]
 import org.testcontainers.containers.GenericContainer;
-// end::GenericContainer[]
 import org.testcontainers.containers.wait.strategy.Wait;
 
-import jakarta.ws.rs.client.ClientBuilder;
-// simple import to build a URI/URL
-import jakarta.ws.rs.core.UriBuilder;
-
-// tag::LibertyContainer[]
 public class LibertyContainer extends GenericContainer<LibertyContainer> {
 
-    // tag::logger[]
-    static final Logger LOGGER = LoggerFactory.getLogger(LibertyContainer.class);
-    // end::logger[]
-
-    private String baseURL;
-
-    private KeyStore keystore;
-    private SSLContext sslContext;
-
-    // tag::getProtocol[]
-    public static String getProtocol() {
-        return System.getProperty("test.protocol", "https");
-    }
-    // end::getProtocol[]
-
-    // tag::testHttps[]
-    public static boolean testHttps() {
-        return getProtocol().equalsIgnoreCase("https");
-    }
-    // end::testHttps[]
-
-    // tag::constructor[]
-    public LibertyContainer(final String dockerImageName) {
-        super(dockerImageName);
+    public LibertyContainer(
+    	String imageName, boolean testHttps, int httpsPort, int httpPort) {
+        
+    	super(imageName);
+        if (testHttps) {
+            addExposedPorts(httpsPort, httpPort);
+        } else {
+            addExposedPorts(httpPort);
+        }
         // wait for smarter planet message by default
         waitingFor(Wait.forLogMessage("^.*CWWKF0011I.*$", 1));
-        init();
-    }
-    // end::constructor[]
 
-    // tag::createRestClient[]
-    public <T> T createRestClient(Class<T> clazz, String applicationPath) {
-        String urlPath = getBaseURL();
-        if (applicationPath != null) {
-            urlPath += applicationPath;
-        }
-        ClientBuilder builder = ResteasyClientBuilder.newBuilder();
-        if (testHttps()) {
-            builder.sslContext(sslContext);
-            builder.trustStore(keystore);
-        }
-        ResteasyClient client = (ResteasyClient) builder.build();
-        ResteasyWebTarget target = client.target(UriBuilder.fromPath(urlPath));
-        return target.proxy(clazz);
     }
-    // end::createRestClient[]
 
     // tag::getBaseURL[]
-    public String getBaseURL() throws IllegalStateException {
-        if (baseURL != null) {
-            return baseURL;
-        }
-        if (!this.isRunning()) {
-            throw new IllegalStateException(
-                "Container must be running to determine hostname and port");
-        }
-        baseURL =  getProtocol() + "://" + this.getHost() + ":" + this.getFirstMappedPort();
-        System.out.println("TEST: " + baseURL);
-        return baseURL;
+    public String getBaseURL(String protocol) throws IllegalStateException {
+    	return protocol + "://" + getHost() + ":" + getFirstMappedPort();
     }
     // end::getBaseURL[]
 
-    // tag::init[]
-    private void init() {
-
-        if (!testHttps()) {
-            // tag::addExposedPorts1[]
-            this.addExposedPorts(9080);
-            // end::addExposedPorts1[]
-            return;
-        }
-
-        // tag::addExposedPorts2[]
-        this.addExposedPorts(9443, 9080);
-        // end::addExposedPorts2[]
-        try {
-            // tag::key-p12[]
-            String keystoreFile = System.getProperty("user.dir")
-                    + "/src/main/liberty/config/resources/security/key.p12";
-            // end::key-p12[]
-            keystore = KeyStore.getInstance("PKCS12");
-            keystore.load(new FileInputStream(keystoreFile), "secret".toCharArray());
-            KeyManagerFactory kmf = KeyManagerFactory.getInstance(
-                                        KeyManagerFactory.getDefaultAlgorithm());
-            kmf.init(keystore, "secret".toCharArray());
-            X509TrustManager xtm = new X509TrustManager() {
-                @Override
-                public void checkClientTrusted(X509Certificate[] chain, String authType)
-                    throws CertificateException { }
-
-                @Override
-                public void checkServerTrusted(X509Certificate[] chain, String authType)
-                    throws CertificateException { }
-
-                @Override
-                public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-            };
-            TrustManager[] tm = new TrustManager[] {
-                                    xtm
-                                };
-            sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(kmf.getKeyManagers(), tm, new SecureRandom());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    // end::init[]
 }
-// end::LibertyContainer[]
-
--- finish/src/test/java/it/io/openliberty/guides/inventory/SystemResourceIT.java
+++ finish/src/test/java/it/io/openliberty/guides/inventory/SystemResourceIT.java
@@ -1,144 +1,14 @@
-// // tag::copyright[]
-// /*******************************************************************************
-//  * Copyright (c) 2022, 2023 IBM Corporation and others.
-//  * All rights reserved. This program and the accompanying materials
-//  * are made available under the terms of the Eclipse Public License 2.0
-//  * which accompanies this distribution, and is available at
-//  * http://www.eclipse.org/legal/epl-2.0/
-//  *
-//  * SPDX-License-Identifier: EPL-2.0
-//  *******************************************************************************/
-// // end::copyright[]
-// package it.io.openliberty.guides.inventory;
-
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import java.util.Base64;
-// import java.util.List;
-
-// import org.junit.jupiter.api.BeforeAll;
-// import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-// import org.junit.jupiter.api.Order;
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.TestMethodOrder;
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.testcontainers.containers.GenericContainer;
-// import org.testcontainers.containers.Network;
-// import org.testcontainers.containers.output.Slf4jLogConsumer;
-// import org.testcontainers.containers.wait.strategy.Wait;
-// import org.testcontainers.junit.jupiter.Container;
-// import org.testcontainers.junit.jupiter.Testcontainers;
-
-// @Testcontainers
-// @TestMethodOrder(OrderAnnotation.class)
-// public class SystemResourceIT {
-
-//     private static Logger logger = LoggerFactory.getLogger(SystemResourceIT.class);
-//     private static String appPath = "/inventory/api";
-//     private static String postgresHost = "postgres";
-//     private static String postgresImageName = "postgres-sample:latest";
-//     private static String appImageName = "inventory:1.0-SNAPSHOT";
-
-//     public static SystemResourceClient client;
-//     // tag::network[]
-//     public static Network network = Network.newNetwork();
-//     // end::network[]
-
-//     // tag::postgresSetup[]
-//     @Container
-//     public static GenericContainer<?> postgresContainer
-//         = new GenericContainer<>(postgresImageName)
-//               // tag::pNetwork[]
-//               .withNetwork(network)
-//               // end::pNetwork[]
-//               .withExposedPorts(5432)
-//               .withNetworkAliases(postgresHost)
-//               .withLogConsumer(new Slf4jLogConsumer(logger));
-//     // end::postgresSetup[]
-
-//     // tag::libertySetup[]
-//     @Container
-//     public static LibertyContainer libertyContainer
-//         = new LibertyContainer(appImageName)
-//               .withEnv("POSTGRES_HOSTNAME", postgresHost)
-//               // tag::lNetwork[]
-//               .withNetwork(network)
-//               // end::lNetwork[]
-//               // tag::health[]
-//               .waitingFor(Wait.forHttp(appPath + "/systems")
-//                               .forPort(9080))
-//               // end::health[]
-//               .withLogConsumer(new Slf4jLogConsumer(logger));
-//     // end::libertySetup[]
-
-//     @BeforeAll
-//     public static void setupTestClass() throws Exception {
-//         System.out.println("TEST: Starting Liberty Container setup");
-//         client = libertyContainer.createRestClient(
-//             SystemResourceClient.class, appPath);
-//     }
-
-//     private void showSystemData(SystemData system) {
-//         System.out.println("TEST: SystemData > "
-//             + system.getId() + ", "
-//             + system.getHostname() + ", "
-//             + system.getOsName() + ", "
-//             + system.getJavaVersion() + ", "
-//             + system.getHeapSize());
-//     }
-
-//     // tag::testAddSystem[]
-//     @Test
-//     @Order(1)
-//     public void testAddSystem() {
-//         System.out.println("TEST: Testing add a system");
-//         // tag::addSystem[]
-//         client.addSystem("localhost", "linux", "11", Long.valueOf(2048));
-//         // end::addSystem[]
-//         // tag::listContents[]
-//         List<SystemData> systems = client.listContents();
-//         // end::listContents[]
-//         assertEquals(1, systems.size());
-//         showSystemData(systems.get(0));
-//         assertEquals("11", systems.get(0).getJavaVersion());
-//         assertEquals(Long.valueOf(2048), systems.get(0).getHeapSize());
-//     }
-//     // end::testAddSystem[]
-
-//     // tag::testUpdateSystem[]
-//     @Test
-//     @Order(2)
-//     public void testUpdateSystem() {
-//         System.out.println("TEST: Testing update a system");
-//         // tag::updateSystem[]
-//         client.updateSystem("localhost", "linux", "8", Long.valueOf(1024));
-//         // end::updateSystem[]
-//         // tag::getSystem[]
-//         SystemData system = client.getSystem("localhost");
-//         // end::getSystem[]
-//         showSystemData(system);
-//         assertEquals("8", system.getJavaVersion());
-//         assertEquals(Long.valueOf(1024), system.getHeapSize());
-//     }
-//     // end::testUpdateSystem[]
-
-//     // tag::testRemoveSystem[]
-//     @Test
-//     @Order(3)
-//     public void testRemoveSystem() {
-//         System.out.println("TEST: Testing remove a system");
-//         // tag::removeSystem[]
-//         client.removeSystem("localhost");
-//         // end::removeSystem[]
-//         List<SystemData> systems = client.listContents();
-//         assertEquals(0, systems.size());
-//     }
-//     // end::testRemoveSystem[]
-// }
-
-
-
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+// end::copyright[]
 package it.io.openliberty.guides.inventory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -148,27 +18,58 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.util.List;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.UriBuilder;
 
-//@Testcontainers
 @TestMethodOrder(OrderAnnotation.class)
 public class SystemResourceIT {
 
-    private static String appPath = "/inventory/api";
-
-    private static String urlPath;
+    private static Logger logger = LoggerFactory.getLogger(SystemResourceIT.class);
     
-    public static SystemResourceClient client;
+    private static int HTTP_PORT = Integer.parseInt(System.getProperty("http.port"));
+    private static int HTTPS_PORT = Integer.parseInt(System.getProperty("https.port"));
+    private static String APP_PATH = System.getProperty("context.root") + "/api";
+    private static String APP_IMAGE = "inventory:1.0-SNAPSHOT";
+
+    private static String POSTGRES_HOST = "postgres";
+    private static int POSTGRES_PORT = 5432;
+    private static String POSTGRES_IMAGE = "postgres-sample:latest";
+
+    private static SystemResourceClient client;
+    private static Network network = Network.newNetwork();
+
+    private static GenericContainer<?> postgresContainer
+        = new GenericContainer<>(POSTGRES_IMAGE)
+              .withNetwork(network)
+              .withExposedPorts(POSTGRES_PORT)
+              .withNetworkAliases(POSTGRES_HOST)
+              .withLogConsumer(new Slf4jLogConsumer(logger));
+
+    private static LibertyContainer inventoryContainer
+        = new LibertyContainer(APP_IMAGE, testHttps(), HTTPS_PORT, HTTP_PORT)
+              .withEnv("POSTGRES_HOSTNAME", POSTGRES_HOST)
+              .withNetwork(network)
+            //   .waitingFor(Wait.forHttp("/health/ready").forPort(HTTP_PORT))
+              .withLogConsumer(new Slf4jLogConsumer(logger));
 
     private static boolean isServiceRunning(String host, int port) {
         try {
@@ -176,7 +77,7 @@ public class SystemResourceIT {
             socket.close();
             return true;
         } catch (Exception e) {
-        	return false;
+            return false;
         }
     }
     
@@ -188,29 +89,56 @@ public class SystemResourceIT {
         return getProtocol().equalsIgnoreCase("https");
     }
     
-    // tag::createRestClient[]
-    private static SystemResourceClient createRestClient() throws KeyStoreException {
+    private static SystemResourceClient createRestClient(String urlPath) throws KeyStoreException {
         ClientBuilder builder = ResteasyClientBuilder.newBuilder();
         if (testHttps()) {
-        	builder.trustStore(KeyStore.getInstance("PKCS12"));
+            builder.trustStore(KeyStore.getInstance("PKCS12"));
+            HostnameVerifier v = new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return hostname.equals("localhost") || hostname.equals("docker");
+                } };
+            builder.hostnameVerifier(v );
         }
         ResteasyClient client = (ResteasyClient) builder.build();
         ResteasyWebTarget target = client.target(UriBuilder.fromPath(urlPath));
         return target.proxy(SystemResourceClient.class);
     }
-    // end::createRestClient[]
 
     @BeforeAll
     public static void setup() throws Exception {
-        if (isServiceRunning("localhost", 5432)) {
-            urlPath = getProtocol() + "://localhost:9443";
+        String urlPath;
+        if (isServiceRunning("localhost", HTTP_PORT)) {
+            logger.info("Testing by dev mode or local runtime...");
+            if (isServiceRunning("localhost", POSTGRES_PORT)) {
+                logger.info("The application is ready to test.");
+                urlPath = getProtocol() + "://localhost:"
+                          + (testHttps() ? HTTPS_PORT : HTTP_PORT);
+            } else {
+                throw new Exception(
+                      "Postgres database is not running");
+            }
         } else {
-            throw new Exception(
-                "Postgres database is not running");
+            logger.info("Testing by using Testcontainers...");
+            if (isServiceRunning("localhost", POSTGRES_PORT)) {
+                throw new Exception(
+                      "Postgres database is running locally. Stop it and retry.");                
+            } else {
+                postgresContainer.start();
+                inventoryContainer.start();
+                urlPath = inventoryContainer.getBaseURL(getProtocol());
+            }
         }
-        urlPath += appPath;
+        urlPath += APP_PATH;
         System.out.println("TEST: " + urlPath);
-        client = createRestClient();
+        client = createRestClient(urlPath);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        inventoryContainer.stop();
+        postgresContainer.stop();
+        network.close();
     }
 
     private void showSystemData(SystemData system) {
