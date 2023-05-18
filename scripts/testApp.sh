027fe7bad159ebea4836e0dbf1364856ee26cf40
Morgan Chang <shin19991207@gmail.com>
2023-05-18 16:42:48 -0400

    update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 set -euxo pipefail
 
-# TEST 1:  Running the application in a Docker container
+# TEST 1:  Running the test by using Testcontainers
+cd ../postgres
+
+docker build -t postgres-sample .
+
+cd ../finish
+
 mvn -ntp -q clean package
 
 docker pull -q icr.io/appcafe/open-liberty:full-java11-openj9-ubi
 
-docker build -t openliberty-getting-started:1.0-SNAPSHOT .
-
-docker run -d --name gettingstarted-app -p 9080:9080 openliberty-getting-started:1.0-SNAPSHOT
+docker build -t inventory:1.0-SNAPSHOT .
 
 sleep 60
 
-docker exec gettingstarted-app cat /logs/messages.log | grep product
-docker exec gettingstarted-app cat /logs/messages.log | grep java
+mvn -ntp verify
 
-status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:9080/dev/system/properties")" 
-if [ "$status" == "200" ]
-then 
-  echo ENDPOINT OK
-else 
-  echo "$status" 
-  echo ENDPOINT NOT OK
-  exit 1
-fi
+# TEST 2: Running the test by local runtime
+docker run --name postgres-container -p 5432:5432 -d postgres-sample
 
-docker stop gettingstarted-app && docker rm gettingstarted-app
-
-# TEST 2: Building and running the application
 mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
@@ -36,26 +29,8 @@ mvn -ntp liberty:start
 mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -Dcontext.root=/dev/ failsafe:integration-test liberty:stop
+    failsafe:integration-test liberty:stop
 mvn -ntp failsafe:verify
 
-# TEST 3: packaging and running the application jar
-mvn -ntp liberty:package -Dinclude=runnable
-if [ ! -f "target/guide-getting-started.jar" ]; then
-    echo "target/guide-getting-started.jar was not generated!"
-    exit 1
-fi
-java -jar target/guide-getting-started.jar &
-GGS_PID=$!
-echo "GGS_PID=$GGS_PID"
-sleep 60
-status="$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:9080/dev/system/properties")"
-kill $GGS_PID
-if [ "$status" == "200" ]
-then
-  echo ENDPOINT OK
-else
-  echo "$status"
-  echo ENDPOINT NOT OK
-  exit 1
-fi
+docker stop postgres-container
+docker rm postgres-container
